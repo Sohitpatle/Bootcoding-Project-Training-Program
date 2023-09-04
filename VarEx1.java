5a7b88bab5dd8971da9fe97694b5e83caa7966dc
Sohitpatle <144015660+Sohitpatle@users.noreply.github.com>
2023-09-04 16:54:13 +0530

    Create VarEx1.java
    One file added

---
+++ VarEx1.java
@@ -0,0 +1,13 @@
+class HelloWorld {
+    public static void main(String[] args) 
+    {
+        int rollno=10;
+        int age=221;
+        double price=1105.55;
+        boolean isFree=true;
+        
+        System.out.println(age);
+        System.out.println(price);
+        System.out.println(isFree);
+    }
+}
